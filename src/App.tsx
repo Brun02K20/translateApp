18e43e1360a6715d93ba501d69d8153b1ed74ba5
Brun02K23 <bvirinni@gmail.com>
2024-03-02 15:56:51 -0300

    Estilizando levemente

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form, Stack } from 'react-bootstrap';
 import './App.css'
 import { useStore } from './hooks/useStore';
 import { AUTO_LANGUAGE } from './constants';
@@ -11,26 +11,43 @@ function App() {
   const {fromLanguage, toLanguage, interchangeLanguages, setFromLanguage, setToLanguage} = useStore()
   return (
       <Container fluid>
-        <h1>Google translate</h1>
+        <h2>Google translate</h2>
         <Row>
           <Col>
-            <LanguageSelect 
+            <Stack gap={2}>
+              <LanguageSelect 
               type={SectionType.From}
               value={fromLanguage}
               onChange={setFromLanguage}
-            />
+              />
+              <Form.Control 
+                as="textarea"
+                placeholder='Ingresar texto'
+                autoFocus
+                style={{height: "150px"}}
+                // value={}
+              />
+            </Stack>
           </Col>
-          <Col>
+          <Col xs="auto">
             <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
               <ArrowsIcon />
             </Button>
           </Col>
           <Col>
-            <LanguageSelect 
+            <Stack gap={2}>
+              <LanguageSelect 
               type={SectionType.To}
               value={toLanguage}
               onChange={setToLanguage}
-            />
+              />
+              <Form.Control 
+                as="textarea"
+                placeholder='Traducción'
+                style={{height: "150px"}}
+                // value={}
+              />
+            </Stack>
           </Col>
         </Row>
       </Container>
